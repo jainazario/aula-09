ad4a48fed9a126ca2e12e5ed361ce1b24884c959
jainazario <nzjaison@gmail.com>
2024-05-06 19:30:19 -0300

    Add files via upload

---
+++ aula8.js
@@ -0,0 +1,26 @@
+aula8.js
+var idade= prompt("digite a sua idade-->")
+
+if (idade>= 18){
+    console.log("voce e maior de idade")
+
+}else{
+
+    //voce e criança -if
+    //voce e adolescente-else if
+    //voce tem 18 anos- else if
+    //voce e adulto-else if
+    //idade invalida -else
+
+    idade1 =prompt("digite sua idade")
+
+    if (idade1>=0,idade1<12) {
+        console.log("voce e criança");
+           
+    }else if(idade1 >=12,idade <18){
+        console.log("voce e adolescente")
+
+
+    }
+}
+
